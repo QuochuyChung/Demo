d1ff75a6b4383c396164bf68ea2a83d2c0113edf
QuochuyChung <chungquochuy2909@gmail.com>
2026-05-26 22:19:19 +0700

    Tạo file product html và sửa bên trong demo dto

--- DemoDTO.java
+++ DemoDTO.java
@@ -6,4 +6,12 @@ public class DemoDTO{
         this.id = id;
         this.name = name;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
